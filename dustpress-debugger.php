befbeed64540f7a0c075c04891dda8515a693586
Timi-Artturi Mäkelä <timi.makela@geniem.com>
2016-04-22 12:34:18 +0300

    added license and composer

--- dustpress-debugger.php
+++ dustpress-debugger.php
@@ -5,6 +5,8 @@ Plugin URI: http://www.geniem.com
 Description: Provides handy ajaxified debugger tool for DustPress based themes.
 Version: 1.0
 Author: Geniem Oy / Miika Arponen & Ville Siltala
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.en.html
 */
 
 class DustPress_Debugger {
