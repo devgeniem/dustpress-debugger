e337917aa63fff39d75e34ea3fd11ec10ff05bd1
Ville Siltala <ville.siltala@geniem.com>
2016-05-27 09:36:41 +0000

    The plugin class now uses namespaces and contains only static functions.

--- plugin.php
+++ plugin.php
@@ -8,9 +8,13 @@
  * Author URI: http://www.geniem.com
  */
 
+namespace DustPress;
+
+use add_action;
+
 class DustPress_Debugger {
 
-	public function __construct() {
+	public static function init() {
 		if ( is_user_logged_in() && current_user_can( "manage_options" ) ) {
 			// Register the debugger script
 			wp_register_script( "dustpress_debugger",  plugin_dir_url( __FILE__ ) .'js/dustpress-debugger.js', ['jquery'], '0.0.2', true );
@@ -20,26 +24,26 @@ class DustPress_Debugger {
 			wp_enqueue_script( "jquery.jsonview", plugin_dir_url( __FILE__ ) .'js/jquery.jsonview.js', ['jquery'], null, true );
 
 			// Register debugger ajax hook
-			add_action( 'wp_ajax_dustpress_debugger', array( $this, 'get_debugger_data' ) );
-			add_action( 'wp_ajax_nopriv_dustpress_debugger', array( $this, 'get_debugger_data' ) );
+			add_action( 'wp_ajax_dustpress_debugger', array( __CLASS__, 'get_debugger_data' ) );
+			add_action( 'wp_ajax_nopriv_dustpress_debugger', array( __CLASS__, 'get_debugger_data' ) );
 
-			add_action( "dustpress/debugger", array( $this, "debugger" ), 1, 1 );
+			add_action( "dustpress/debugger", array( __CLASS__, "debugger" ), 1, 1 );
 		}
 	}
 
-	public function debugger( $hash ) {	
+	public static function debugger( $hash ) {
 
 		$data_array = array(
 			'ajaxurl' 	=> admin_url( 'admin-ajax.php' ),
 			'hash' 		=> $hash
 		);
-		
+
 		wp_localize_script( 'dustpress_debugger', 'dustpress_debugger', $data_array );
 
 		wp_enqueue_script( 'dustpress_debugger' );
 	}
 
-	public function get_debugger_data() {
+	public static function get_debugger_data() {
 		if ( defined("DOING_AJAX") ) {
 			session_start();
 
@@ -65,4 +69,4 @@ class DustPress_Debugger {
 	}
 }
 
-new DustPress_Debugger();
+add_action( 'init', __NAMESPACE__ . '\\DustPress_Debugger::init' );
