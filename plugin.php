93b3fa45d6bfbe3b64a8e40c5ba8e62a89098bf3
Miika Arponen <miika@geniem.com>
2016-06-21 15:19:54 +0300

    Changed deprecated update_usermeta to update_user_meta

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: DustPress Debugger
  * Plugin URI: https://github.com/devgeniem/dustpress-debugger
  * Description: Provides handy ajaxified debugger tool for DustPress based themes.
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: Geniem Oy / Miika Arponen & Ville Siltala
  * Author URI: http://www.geniem.com
  */
@@ -181,7 +181,7 @@ class Debugger {
             return false;
         }
 
-        update_usermeta( $user_id, "dustpress_debugger", $_POST["dustpress_debugger"] );
+        update_user_meta( $user_id, "dustpress_debugger", $_POST["dustpress_debugger"] );
     }
 }
 
