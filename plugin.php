a3d31dd70e7bdff7d48d35d348e9f1af94e5dd9e
Miika Arponen <miika@geniem.com>
2020-02-20 15:21:31 +0200

    Version 1.5.1

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: DustPress Debugger
  * Plugin URI: https://github.com/devgeniem/dustpress-debugger
  * Description: Provides handy ajaxified debugger tool for DustPress based themes.
- * Version: 1.5.0
+ * Version: 1.5.1
  * Author: Geniem Oy / Miika Arponen & Ville Siltala
  * Author URI: http://www.geniem.com
  */
